96f3bfe62b7475d4d9c728094bb8e80bc138747b
Talha Cetin <talhacetin01@hotmail.com>
2023-12-28 21:43:46 +0300

    adding event create form

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -6,7 +6,7 @@ import {
     SelectValue,
 } from "@/components/ui/select"
 import { ICategory } from "@/lib/database/models/category.model"
-import { startTransition, useState } from "react"
+import { startTransition, useEffect, useState } from "react"
 import {
     AlertDialog,
     AlertDialogAction,
@@ -19,6 +19,7 @@ import {
     AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { Input } from "../ui/input"
+import { createCategory, getAllCategories } from "@/lib/actions/category.actions"
 
 
 type DropdownProps = {
@@ -32,8 +33,23 @@ const Dropdown = ({ onChangeHandler, value }: DropdownProps) => {
     const [newCategory, setNewCategory] = useState('')
 
     const handleAddCategory = () => {
-        
+        createCategory({
+            categoryName: newCategory.trim()
+        })
+            .then((category) => {
+                setCategories((prevState) => [...prevState, category])
+            })
     }
+
+    useEffect(() => {
+        const getCategories = async () => {
+            const categoryList = await getAllCategories()
+
+            categoryList && setCategories(categoryList as ICategory[])
+        }
+
+        getCategories();
+    }, [])
 
 
     return (
@@ -48,7 +64,7 @@ const Dropdown = ({ onChangeHandler, value }: DropdownProps) => {
                     </SelectItem>
                 ))}
                 <AlertDialog>
-                    <AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">Open</AlertDialogTrigger>
+                    <AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">New</AlertDialogTrigger>
                     <AlertDialogContent
                         className="bg-white"
                     >
@@ -56,7 +72,7 @@ const Dropdown = ({ onChangeHandler, value }: DropdownProps) => {
                             <AlertDialogTitle>New Category</AlertDialogTitle>
                             <AlertDialogDescription>
                                 <Input type="text" placeholder="Category name" className="input-field mt-3"
-                                onChange={(e) => setNewCategory(e.target.value)}/>
+                                    onChange={(e) => setNewCategory(e.target.value)} />
                             </AlertDialogDescription>
                         </AlertDialogHeader>
                         <AlertDialogFooter>
